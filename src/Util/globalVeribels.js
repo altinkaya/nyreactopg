42a4325cbc94378eb940ac09e99e19a5ad7f284b
altinkaya <124771353+altinkaya@users.noreply.github.com>
2024-05-15 12:39:38 +0200

    Update globalVeribels.js

--- src/Util/globalVeribels.js
+++ src/Util/globalVeribels.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:7000/api';
-export const BASE_URL_DEV  = 'https://api.altinkaya.dk/api';
+export const BASE_URL_DEV = 'http://localhost:7000/api';
+export const BASE_URL  = 'https://api.altinkaya.dk/api';
